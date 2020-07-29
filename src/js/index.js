4f93f2f365a6b2fc9aa09e13ebbd0e624d5b0931
kev bonett <kev.bonett@geronimoweb.com>
2020-07-29 13:53:36 +0100

    upgrade to Parcel2, try to fix IE11

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 import '../scss/index.scss';
-import '../icons/svg-icon-defs.svg';
 
 import cssVars from 'css-vars-ponyfill';
 
@@ -8,4 +7,4 @@ cssVars();
 
 /* eslint-disable no-alert, no-console */
 const hello = 'ES6 const works!';
-alert(hello);
+console.log(hello);
